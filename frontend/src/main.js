84517f76a2ccf698b005e57bc24bb218b9ca8778
neyugn261 <quocnguyen191112@gmail.com>
2025-04-12 01:23:30 +0700

    Fix UI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,8 +6,11 @@ import GoogleLoginPlugin from "vue3-google-login";
 
 const app = createApp(App);
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+// const facebookAppId = import.meta.env.VITE_FACEBOOK_APP_ID;
 app.use(GoogleLoginPlugin, {
     clientId: googleClientId,
 });
 app.use(router);
 app.mount("#app");
+
+// window.FB_APP_ID = facebookAppId;
