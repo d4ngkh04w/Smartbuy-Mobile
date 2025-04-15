61acda7af4d79ad5e995d679ac74700bc256da0b
neyugn261 <quocnguyen191112@gmail.com>
2025-04-15 22:20:55 +0700

    Modify LoginModal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import "./assets/base.css"; // Import CSS chung
 import GoogleLoginPlugin from "vue3-google-login";
 
+
 const app = createApp(App);
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 // const facebookAppId = import.meta.env.VITE_FACEBOOK_APP_ID;
