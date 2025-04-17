3c8ba0f4c66888bc398ed8063f5aca11afcfadee
neyugn261 <quocnguyen191112@gmail.com>
2025-04-18 00:38:45 +0700

    Add forget password

--- frontend/client/src/router/index.js
+++ frontend/client/src/router/index.js
@@ -1,19 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/HomeView/Home.vue';
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/Home.vue";
+import ResetPasswordPage from "../views/ResetPasswordPage.vue";
 
 const routes = [
     {
-        path: '/',
-        name: 'Home',
-        component: Home
+        path: "/",
+        name: "home",
+        component: HomeView,
     },
-  
+    {
+        path: "/reset-password",
+        name: "resetPassword",
+        component: ResetPasswordPage,
+        meta: { requiresAuth: false },
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            return {
+                el: to.hash,
+                behavior: "smooth",
+            };
+        } else if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0 };
+        }
+    },
 });
 
 export default router;
